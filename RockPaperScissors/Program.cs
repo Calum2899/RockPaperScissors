93bf52e103fcf6763522b03e21560d6221c170c4
Calum Burgess <calumburgess99@gmail.com>
2025-06-27 19:29:39 +0100

    Added Last Choice Computer

--- RockPaperScissors/Program.cs
+++ RockPaperScissors/Program.cs
@@ -5,9 +5,33 @@
         static void Main(string[] args)
         {
 
+            Console.WriteLine("Welcome to Rock, Paper, Scissors!");
+            Console.WriteLine("Enter 0 at any time to come back to this screen or to exit from this screen");
+            Console.WriteLine("Choose Gamemode: [1]Normal [2]Last Choice Computer ");
+            var gameMode = Console.ReadLine();
+            switch (gameMode)
+            {
+                case "0":
+                    Environment.Exit(0);
+                    break;
+                case "1":
+                    var normalGame = new RockPaperScissors();
+                    PlayNormal(normalGame);
+                    break;
+                case "2":
+                    var playLastChoiceGame = new RockPaperScissors();
+                    PlayLastChoiceComputer(playLastChoiceGame);
+                    break;
+                default:
+                    Console.WriteLine("Invalid choice. Please select 1 or 2. (0 to exit)");
+                    Main(new string[] { });
+                    break;
+            }
+        }
 
-            RockPaperScissors game = new RockPaperScissors();
-            Console.WriteLine("Select [1]Rock [2]Paper [3]Scissors");
+        private static void PlayNormal(RockPaperScissors game)
+        {
+            Console.WriteLine("Select: [1]Rock [2]Paper [3]Scissors");
 
             var userSelection = Console.ReadLine();
             var playerChoice = string.Empty;
@@ -15,6 +39,7 @@
             {
                 playerChoice = userSelection switch
                 {
+                    "0" => "Back",
                     "1" => "Rock",
                     "2" => "Paper",
                     "3" => "Scissors",
@@ -24,16 +49,50 @@
             catch (ArgumentException ex)
             {
                 Console.WriteLine("Please select valid option by entering the desired number");
-                Main(args);
+                PlayNormal(game);
             }
-
-            game.Play(playerChoice);
+            if (playerChoice == "Back")
+                Main(new string[] { });
+            game.PlayNormal(playerChoice);
             // Display the result
             Console.WriteLine($"Player choice: {playerChoice}");
             Console.WriteLine($"Computer choice: {game.computer}");
             Console.WriteLine($"Result: {game.result}");
 
-            Main(args);
+            PlayNormal(game);
+        }
+
+        private static void PlayLastChoiceComputer(RockPaperScissors game)
+        {
+            Console.WriteLine("Select: [1]Rock [2]Paper [3]Scissors");
+
+            var userSelection = Console.ReadLine();
+            var playerChoice = string.Empty;
+            try
+            {
+                playerChoice = userSelection switch
+                {
+                    "0" => "Back",
+                    "1" => "Rock",
+                    "2" => "Paper",
+                    "3" => "Scissors",
+                    _ => throw new ArgumentException("Invalid choice. Please select 1, 2, or 3.")
+                };
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine("Please select valid option by entering the desired number");
+                PlayLastChoiceComputer(game);
+            }
+            if (playerChoice == "Back")
+                Main(new string[] { });
+            game.PlayLastChoiceComputer(playerChoice);
+            // Display the result
+            Console.WriteLine($"Player choice: {playerChoice}");
+            Console.WriteLine($"Computer choice: {game.computer}");
+            Console.WriteLine($"Result: {game.result}");
+
+            PlayLastChoiceComputer(game);
         }
     }
 }
--- RockPaperScissors/RockPaperScissors.cs
+++ RockPaperScissors/RockPaperScissors.cs
@@ -10,41 +10,44 @@ namespace RockPaperScissors
     {
         public string computer { private set; get; } = string.Empty;
         public string player = string.Empty;
+        public string playerLastChoice = string.Empty;
 
 
         public string result { private set; get; } = string.Empty;
         private string[] choices = { "Rock", "Paper", "Scissors" };
 
-        public void Play(string playerChoice)
+        public void PlayNormal(string playerChoice)
         {
             computer = choices[new Random().Next(choices.Length)];
             result = GetResult(playerChoice, computer);
         }
+        public void PlayLastChoiceComputer(string playerChoice)
+        {
+            
+            if (computer == string.Empty)
+                computer = choices[new Random().Next(choices.Length)];
+            else
+            {
+                computer = playerLastChoice;
+            }
+                result = GetResult(playerChoice, computer);
+            
+        }
         public string GetResult(string playerOneChoice, string playerTwoChoice)// player two assume is computer so only gives result for player one
         {
             if (string.IsNullOrEmpty(playerOneChoice))
-            {
                 throw new ArgumentNullException(nameof(playerOneChoice), "Player one's choice cannot be null or empty.");
-            }
             if (string.IsNullOrEmpty(playerTwoChoice))
-            {
                 throw new ArgumentNullException(nameof(playerTwoChoice), "Player two's choice cannot be null or empty.");
-            }
-
+            playerLastChoice = playerOneChoice;
             if (playerOneChoice == playerTwoChoice)
-            {
                 return "Tie";
-            }
             else if ((playerOneChoice == "Rock" && playerTwoChoice == "Scissors") ||
                      (playerOneChoice == "Paper" && playerTwoChoice == "Rock") ||
                      (playerOneChoice == "Scissors" && playerTwoChoice == "Paper"))
-            {
                 return "Winner";
-            }
             else
-            {
                 return "Loser";
-            }
         }
     }
 }
